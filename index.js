86d3645e402bd82b9123ffc2a3a84197678a47a0
PatrickFHam <93452044+PatrickFHam@users.noreply.github.com>
2022-03-08 16:03:42 -0500

    moved classes into lib folder

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const Employee = require('./Employee');
-const Manager = require('./Manager');
-const Engineer = require('./Engineer');
-const Intern = require('./Intern');
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
