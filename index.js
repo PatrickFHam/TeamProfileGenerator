7a8eaf3602c95b9d236de7ba9e532403b6db4653
PatrickFHam <93452044+PatrickFHam@users.noreply.github.com>
2022-03-09 11:52:15 -0500

    buildHTML now outputs a working array, removed unncessary console logs

--- index.js
+++ index.js
@@ -6,55 +6,132 @@ const Intern = require('./lib/Intern');
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-teamMembers = [];
+let teamMembers = [];
 
-const collectManagerInputs = async (inputs = []) => {
-  const prompts = [
+const buildHTML = (teamMembers) => {
+  
+  console.log("HTML will be built now, using the following array:");
+  console.log(teamMembers);
+}
+
+const addIntern = async (teamMembers) => {
+
+  const newInternPrompts = [
+    {
+      type: 'input',
+      name: 'internName',
+      message: "What is the Intern's NAME? "
+    },
+    {
+      type: 'input',
+      name: 'internId',
+      message: "What is the Intern's ID? "
+    },
+    {
+      type: 'input',
+      name: 'internEmail',
+      message: "What is the Intern's EMAIL? "
+    },
+    {
+      type: 'input',
+      name: 'internSchool',
+      message: "What is the Intern's SCHOOL? "
+    }
+  ]
+
+  let newIntern = await inquirer.prompt(newInternPrompts);
+
+  let intern = new Intern (newIntern.internName, newIntern.internId, newIntern.internEmail, newIntern.internSchool);
+
+  teamMembers.push(intern);
+
+  qToAddNewTeamMember(teamMembers);
+}
+
+const addEngineer = async (teamMembers) => {
+
+  const newEngineerPrompts = [
+    {
+      type: 'input',
+      name: 'engineerName',
+      message: "What is the Engineer's NAME? "
+    },
+    {
+      type: 'input',
+      name: 'engineerId',
+      message: "What is the Engineer's ID? "
+    },
+    {
+      type: 'input',
+      name: 'engineerEmail',
+      message: "What is the Engineer's EMAIL? "
+    },
+    {
+      type: 'input',
+      name: 'engineerGithub',
+      message: "What is the Engineer's GITHUB USERNAME? "
+    }
+  ]
+
+  const newEngineerInfo = await inquirer.prompt(newEngineerPrompts);
+
+  const engineer = new Engineer (newEngineerInfo.engineerName, newEngineerInfo.engineerId, newEngineerInfo.engineerEmail, newEngineerInfo.engineerGithub);
+
+  teamMembers.push(engineer);
+
+  qToAddNewTeamMember(teamMembers);
+}
+
+const qToAddNewTeamMember = async (teamMembers) => {
+  const newTeamMemberPrompts = [
+    {
+      type: 'list',
+      name: 'chooseToAddAnother',
+      message: "Which would you like to do? ",
+      choices: ["Add an ENGINEER to the team.", "Add an INTERN to the team.", "BUILD the Team Page."]
+    },
+  ];
+
+  let choice = await inquirer.prompt(newTeamMemberPrompts);
+
+  if (choice.chooseToAddAnother == "Add an ENGINEER to the team.") {return addEngineer(teamMembers)} else
+  if (choice.chooseToAddAnother == "Add an INTERN to the team.")   {return addIntern(teamMembers)}   else
+  {return buildHTML(teamMembers)};
+}
+
+const getManagerInfo = async () => {
+  const managerPrompts = [
     {
       type: 'input',
       name: 'managerName',
-      message: "What is the name of the team's Manager? "
+      message: "What is the Manager's NAME? "
     },
     {
       type: 'input',
       name: 'managerId',
-      message: "What is the ID of the team's Manager? "
+      message: "What is the Manager's ID? "
     },
     {
       type: 'input',
       name: 'managerEmail',
-      message: "What is the Manager's email address? "
+      message: "What is the Manager's EMAIL? "
     },
     {
       type: 'input',
       name: 'managerOfficeNumber',
-      message: "What is the Manager's office number? (phone extension, 3-digits) "
-    },
-    {
-      type: 'confirm',
-      name: 'addNewTeamMember',
-      message: 'Would you like to ADD ANOTHER MANAGER MEMBER? ',
-      default: true
+      message: "What is the Manager's OFFICE NUMBER (ext., 3-digit-#) "
     }
   ];
 
-  const { addNewTeamMember, ...answers } = await inquirer.prompt(prompts);
-  const newInputs = [...inputs, answers];
-  console.log(answers);
-  console.log(answers.managerName);
-  console.log(answers.managerEmail);
-  console.log(answers.managerOfficeNumber);
-
-  return addNewTeamMember ? collectTeamMemberInputs(newInputs) : newInputs;
+  const managerInfo = await inquirer.prompt(managerPrompts);
+  return managerInfo;
 };
 
 const main = async () => {
-  const managerInputs = await collectManagerInputs();
-  console.log(managerInputs);
-  
-  
-  const teamMemberInputs = await collectTeamMemberInputs();
-  console.log(teamMemberInputs);
+  const managerInfo = await getManagerInfo();
+  const manager = new Manager (managerInfo.managerName, managerInfo.managerId, managerInfo.managerEmail, managerInfo.managerOfficeNumber);
+  teamMembers.push(manager);
+  await qToAddNewTeamMember(teamMembers);
 };
 
 main();
