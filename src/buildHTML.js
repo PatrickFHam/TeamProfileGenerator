d76c219b5b99685b3ddcb524119644b99052e13a
PatrickFHam <93452044+PatrickFHam@users.noreply.github.com>
2022-03-11 14:57:13 -0500

    fixed tab-spacing in generated HTML

--- src/buildHTML.js
+++ src/buildHTML.js
@@ -82,48 +82,47 @@ buildHTML = (teamMembers) => {
 };
 
 function buildWholePage (htmlOfAllEmployeeCards) {
-  html = `
-    <!doctype html>
-    <html lang="en">
-      <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+  html = 
+`<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="stylesheet" href="./assets/css/reset.css">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/reset.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/style.css">
 
-        <title>My Team</title>
-      </head>
-      <body>
-        
-        <header class="bg-image d-flex flex-wrap justify-content-center py-3 border-bottom mx-0" style="background-image: url(./assets/images/headerbg.jpg)">
-          <a href="" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-            <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-            <h1 class="fs-4 btn btn-light btn-lg">My Team</h1>
-          </a>
+    <title>My Team</title>
+  </head>
+  <body>
+    
+    <header class="bg-image d-flex flex-wrap justify-content-center py-3 border-bottom mx-0" style="background-image: url(./assets/images/headerbg.jpg)">
+      <a href="" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+        <h1 class="fs-4 btn btn-light btn-lg">My Team</h1>
+      </a>
 
-          <ul class="nav nav-pills">
-            <li class="nav-item"><a href="https://www.github.com/patrickfham" target="_blank" class="nav-link">About Me</a></li>
-          </ul>
-        </header>
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="https://www.github.com/patrickfham" target="_blank" class="nav-link">About Me</a></li>
+      </ul>
+    </header>
 
-        <main>
-          <div id="mainbox" class="container d-flex flex-wrap justify-content-center my-3">
+    <main>
+      <div id="mainbox" class="container d-flex flex-wrap justify-content-center my-3">
 
-            ${htmlOfAllEmployeeCards}
+        ${htmlOfAllEmployeeCards}
 
-          </div>
+      </div>
 
-        </main>
+    </main>
 
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-        <script src="./assets/js/script.js"></script>
-      </body>
-    </html>
-  `
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <script src="./assets/js/script.js"></script>
+  </body>
+</html>`
 
   return html;
 };
