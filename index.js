ee25dfe1b316ca69295cfcfe216c677fbc12d30b
PatrickFHam <93452044+PatrickFHam@users.noreply.github.com>
2022-03-11 12:34:22 -0500

    fixed a typo in a question

--- index.js
+++ index.js
@@ -250,7 +250,7 @@ const getManagerInfo = async () => {
     {
       type: 'input',
       name: 'managerOfficeNumber',
-      message: "What is the Manager's OFFICE PHONE NUMBER",
+      message: "What is the Manager's OFFICE PHONE NUMBER? ",
       // PHONE NUMBER VALIDATION 'if' came from RegexTester.com
       validate: function(managerOfficeNumber) {
         officeNumberIsValid = /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/g.test(managerOfficeNumber);
