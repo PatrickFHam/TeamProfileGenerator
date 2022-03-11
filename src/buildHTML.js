d3fa2ee0777ff2efea939b59fa16b585283be812
PatrickFHam <93452044+PatrickFHam@users.noreply.github.com>
2022-03-11 14:43:58 -0500

    flipped the card formatting, name and title

--- src/buildHTML.js
+++ src/buildHTML.js
@@ -3,8 +3,8 @@ function createManagerCard (manager) {
     <div class="card border-primary manager-card mx-2" style="width: 18rem;">
       <img src="./assets/images/managerlogo.jpg" class="card-img-top" alt="managerlogo">
       <div class="card-body">
-        <h3 class="card-title role-on-card role-is-manager">Project Manager</h3>
-        <h5 class="card-text name-on-card">${manager.name}</h5>
+        <h3 class="card-title name-on-card">${manager.name}</h3>
+        <h5 class="card-text role-on-card role-is-manager">Project Manager</h5>
         <p class="card-text id-on-card">ID#: ${manager.id}</p>
         <p class="card-text email-on-card">${manager.email}</p>
         <p class="card-text office-number-on-card">Office Number: ${manager.officeNumber}</p>
@@ -20,8 +20,8 @@ function createEngineerCard (engineer) {
   <div class="card border-danger engineer-card mx-2" style="width: 18rem;">
     <img src="./assets/images/engineerlogo.jpg" class="card-img-top" alt="engineerlogo">
     <div class="card-body">
-      <h3 class="card-title role-on-card role-is-engineer">Engineer</h3>
-      <h5 class="card-text name-on-card">${engineer.name}</h5>
+      <h3 class="card-title name-on-card">${engineer.name}</h3>
+      <h5 class="card-text role-on-card role-is-engineer">Engineer</h5>
       <p class="card-text id-on-card">ID#: ${engineer.id}</p>
       <p class="card-text email-on-card">${engineer.email}</p>
       <p class="card-text github-on-card">GitHub: ${engineer.github}</p>
@@ -38,8 +38,8 @@ function createInternCard (intern) {
     <div class="card border-dark intern-card mx-2" style="width: 18rem;">
       <img src="./assets/images/internlogo.jpg" class="card-img-top" alt="internlogo">
       <div class="card-body">
-        <h3 class="card-title role-on-card role-is-intern">Intern</h3>
-        <h5 class="card-text name-on-card">${intern.name}</h5>
+        <h3 class="card-title name-on-card">${intern.name}</h3>
+        <h5 class="card-text role-on-card role-is-intern">${intern.name}</h5>
         <p class="card-text id-on-card">ID#: ${intern.id}<p>
         <p class="card-text email-on-card">${intern.email}</p>
         <p class="card-text school-on-card">School: ${intern.school}</p>
